ca223fe70ff9d8488178e5d76f4e426b08e31e0b
salehelhawaryy <salehmohamad210@gmail.com>
2023-10-05 20:38:09 +0300

    package model + adminController functions

--- api/routes/adminRoutes.js
+++ api/routes/adminRoutes.js
@@ -1,10 +1,15 @@
-import express from 'express'
+import express, { Router } from 'express'
 const router = express.Router()
 import adminController from '../controllers/adminController.js'
 
-
-router.post('/addAdmin', adminController.addAdmin)
+router.get('/getAllAdmins', adminController.getAllAdmins)
+router.get('/getAllDoctors', adminController.getAllDoctors)
+router.get('/getAllPatients', adminController.getAllPatients)
 router.get('/getUser/:id', adminController.getUser)
+router.get('/getDoctorRequest/:id', adminController.getDoctorRequest)
+router.get('/getAllDoctorRequest', adminController.getAllDoctorRequest)
+router.post('/addAdmin', adminController.addAdmin)
+router.delete('/deleteUser/:id', adminController.removeUser)
 //router.get('/getUsers', )
 
 
