57075986514f9005ef49fe1ee4b20546bf154aa4
salehelhawaryy <salehmohamad210@gmail.com>
2023-10-05 18:19:30 +0300

    admin schema + routes

--- api/app.js
+++ api/app.js
@@ -5,10 +5,11 @@
 import express from 'express'
 import cors from 'cors'
 import userRoutes from './routes/userRoutes.js'
+import adminRoutes from './routes/adminRoutes.js'
 import { connectToDatabase } from './database.js'
 
 const app = express()
-const port = process.env.PORT || 10000
+const port = process.env.PORT || 3000
 
 const corsOptions = {
     origin: ['http://127.0.0.1:5174', 'http://localhost:5174'],
@@ -16,8 +17,10 @@ const corsOptions = {
     credentials: true,
 }
 
+app.use(express.json());
 app.use(cors(corsOptions))
 app.use('/api/users', userRoutes)
+app.use('/api/admin', adminRoutes)
 
 // --------------------------------------------------
 // Mongoose
