d800830799b768db4a93a196db1e6e571161ee5a
OMAR-AHMED-SAAD <110069095+OMAR-AHMED-SAAD@users.noreply.github.com>
2023-10-06 20:19:36 +0300

    fixed patientCard page

--- client/src/components/PatientCard/PatientCard.jsx
+++ client/src/components/PatientCard/PatientCard.jsx
@@ -1,5 +1,6 @@
 import './patientCard.css'
 import { useNavigate } from 'react-router-dom'
+import { calcAge } from '../PatientInfo/PatientInfo'
 
 const PatientCard = ({ patient }) => {
     const navigate = useNavigate()
@@ -9,10 +10,10 @@ const PatientCard = ({ patient }) => {
     return (
         <div className='patient-card' onClick={handlePatientSelect}>
             <h3>{patient.name}</h3>
-            <p>Age: {patient.age}</p>
-            <p>Contact: {patient.contact}</p>
-            <p>Last Visit: {patient.lastVisit}</p>
-            <p>Next Appointment: {patient.nextAppointment}</p>
+            <p><strong>Age: </strong>{calcAge(patient.birthdate)}</p>
+            <p><strong>Gender: </strong>{patient.gender}</p>
+            <p><strong>Last Visit: </strong>{patient.lastVisit|| "No previous visits"}</p>
+            <p><strong>Next Appointment: </strong>{patient.nextAppointment|| "No upcoming appointments"}</p>
             <button className='view-records-btn button'>View Records</button>
         </div>
     )
--- client/src/components/PatientInfo/PatientInfo.jsx
+++ client/src/components/PatientInfo/PatientInfo.jsx
@@ -3,6 +3,21 @@ import './patientInfo.css'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
 
+export const calcAge = (birthdate) => {
+    birthdate = new Date(birthdate)
+    const currentDate = new Date()
+    const age = currentDate.getFullYear() - birthdate.getFullYear()
+
+    // Check if the current date has passed the birthday this year
+    const hasBirthdayPassed =
+        currentDate.getMonth() > birthdate.getMonth() ||
+        (currentDate.getMonth() === birthdate.getMonth() &&
+            currentDate.getDate() >= birthdate.getDate())
+
+    // If the birthday hasn't occurred this year, subtract 1 from the age
+    return hasBirthdayPassed ? age : age - 1
+}
+
 const PatientInfoComponent = () => {
     const [SelectedPatient, setSelectedPatient] = useState({})
     const [downloadMessage, setDownloadMessage] = useState('')
@@ -16,21 +31,6 @@ const PatientInfoComponent = () => {
                 setSelectedPatient(res.data)
             })
     }, [id])
-
-    const calcAge = (birthdate) => {
-        birthdate = new Date(birthdate)
-        const currentDate = new Date()
-        const age = currentDate.getFullYear() - birthdate.getFullYear()
-
-        // Check if the current date has passed the birthday this year
-        const hasBirthdayPassed =
-            currentDate.getMonth() > birthdate.getMonth() ||
-            (currentDate.getMonth() === birthdate.getMonth() &&
-                currentDate.getDate() >= birthdate.getDate())
-
-        // If the birthday hasn't occurred this year, subtract 1 from the age
-        return hasBirthdayPassed ? age : age - 1
-    }
 
     const axiosDownloadFile = (fileName) => {
         setDownloadMessage('')
