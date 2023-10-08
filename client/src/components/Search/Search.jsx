532707c89482eeb47d616bdd2d2c5af106b01b7e
Ahmed <ahmedmahmoudkzm@gmail.com>
2023-10-08 22:32:29 +0300

    Added filters inputs, modified search for patient's home

--- client/src/components/Search/Search.jsx
+++ client/src/components/Search/Search.jsx
@@ -4,7 +4,7 @@ import './search.css'
 const Search = ({ onSearch }) => {
     const [searchTerm, setSearchTerm] = useState('')
     const onSearchTermChange = (e) => {
-        const searchString = e.target.value.replace(/[^a-z]/gi, '')
+        const searchString = e.target.value.replace(/[^a-z\s]/gi, '')
         setSearchTerm(searchString)
         onSearch(searchString)
     }
--- client/src/pages/PatientHome/PatientHome.jsx
+++ client/src/pages/PatientHome/PatientHome.jsx
@@ -2,23 +2,46 @@ import { useEffect, useState } from 'react'
 import axios from 'axios'
 import Search from '../../components/Search/Search'
 import Pagination from '../../components/Pagination/Pagination'
+import { DatePicker, Select } from 'antd'
 
 import { generateDummyData } from '../../utils/generateDummyData'
+import { findIntersection } from '../../utils/intersectionForSearch'
 
 import './patientHome.css'
 import DoctorCard from '../../components/DoctorCard/DoctorCard'
 
+const { RangePicker } = DatePicker
+
 const PatientHome = () => {
     // REQ 37 | View all doctors, DONE
     // REQ 38 | search by name or speciality, DONE
-    // REQ 39 | filter by speciality and or availability
+    // REQ 39 | filter by speciality and or availability, IN PROGRESS
     // REQ 40 | navigate to selected doctor
-
     const [doctors, setDoctors] = useState(null)
     const [searchTerm, setSearchTerm] = useState('')
     const [currentPage, setCurrentPage] = useState(1)
     const [filteredDoctors, setFilteredDoctors] = useState(null)
+    const [specialties, setSpecialties] = useState([])
     const doctorsPerPage = 8
+
+    useEffect(() => {
+        let options = []
+        if (filteredDoctors?.length > 0) {
+            filteredDoctors.forEach((doctor) => {
+                if (!options.some((obj) => obj.value === doctor.speciality)) {
+                    options.push({
+                        label: doctor.speciality,
+                        value: doctor.speciality,
+                    })
+                }
+            })
+        }
+        setSpecialties(options)
+    }, [filteredDoctors])
+
+    const handleChange = (value) => {
+        console.log(`selected ${value}`)
+    }
 
     useEffect(() => {
         axios
@@ -36,14 +59,18 @@ const PatientHome = () => {
             setFilteredDoctors(
                 doctors.filter(
                     (doctor) =>
-                        doctor.name
-                            ?.toLowerCase()
-                            .includes(searchTerm.toLowerCase()) ||
-                        doctor.speciality
-                            ?.toLowerCase()
-                            .includes(searchTerm.toLowerCase())
+                        findIntersection(
+                            searchTerm.split(' '),
+                            doctor.name.split(' ')
+                        ) ||
+                        findIntersection(
+                            searchTerm.split(' '),
+                            doctor.speciality.split(' ')
+                        ) ||
+                        searchTerm === ''
                 )
             )
+            console.log(searchTerm.split(' '), doctors)
             setCurrentPage(1)
         }
     }, [searchTerm, doctors])
@@ -70,13 +97,28 @@ const PatientHome = () => {
         <section className='doctor-list-conatiner'>
             <h2>Doctors</h2>
             <Search onSearch={onSearch} />
+            <div id='doctors-filters'>
+                <RangePicker
+                    showTime={{
+                        format: 'hh:mm a',
+                    }}
+                    format='YYYY-MM-DD hh:mm a'
+                />
+                <Select
+                    mode='multiple'
+                    allowClear
+                    placeholder='Select specialities'
+                    onChange={handleChange}
+                    options={specialties}
+                />
+            </div>
             <div className='doctor-list'>{getDoctors()}</div>
             <Pagination
                 itemsPerPage={doctorsPerPage}
                 totalItems={filteredDoctors?.length}
                 paginate={(pageNumber) => setCurrentPage(pageNumber)}
                 currentPage={currentPage}
-            />{' '}
+            />
             <button
                 onClick={() => {
                     generateDummyData(10, 'doctor')
