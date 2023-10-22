d2493af645d7f721899f10d66e0dc1beead843c0
OMAR-AHMED-SAAD <110069095+OMAR-AHMED-SAAD@users.noreply.github.com>
2023-10-22 17:55:43 +0300

    fixed imageGallery

--- client/src/components/reusable/ImageGallery/ImageGallery.jsx
+++ client/src/components/reusable/ImageGallery/ImageGallery.jsx
@@ -1,56 +1,63 @@
 // src/components/ImageGallery.js
-import React, { useState } from 'react';
-import { Modal, Button, Row, Col } from 'antd';
-import './imageGallery.css';
-
-const ImageGallery = ({images}) => {
-    const [visible, setVisible] = useState(false);
-    const [selectedImage, setSelectedImage] = useState(null);
-
-    // const images = [
-    //     'https://via.placeholder.com/200',
-    //     'https://via.placeholder.com/200',
-    //     'https://via.placeholder.com/200',
-    //     // Add more image URLs as needed
-    // ];
+import React, { useState } from 'react'
+import { Modal, Button, Spin, Col } from 'antd'
+import './imageGallery.css'
+import noImage from '/src/assets/imgs/noImage.png'
+const ImageGallery = ({ images }) => {
+    const [visible, setVisible] = useState(false)
+    const [selectedImage, setSelectedImage] = useState(null)
+    const [imageLoadingStates, setImageLoadingStates] = useState(
+        Array(images?.length).fill(true)
+    )
 
     const handleImageClick = (index) => {
-        setSelectedImage(index);
-        setVisible(true);
-    };
+        setSelectedImage(index)
+        setVisible(true)
+    }
 
     const handleClose = () => {
-        setVisible(false);
-        setSelectedImage(null);
-    };
+        setVisible(false)
+        setSelectedImage(null)
+    }
 
     return (
-        <div className="image-gallery-container">
-            <Row gutter={16}>
-                {images.map((imageUrl, index) => (
-                    <Col key={index} span={6}>
-                        <img
-                            src={imageUrl}
-                            alt={`Image ${index}`}
-                            className="gallery-image"
-                            onClick={() => handleImageClick(index)}
-                        />
-                    </Col>
-                ))}
-            </Row>
-            <Modal
-                visible={visible}
-                onCancel={handleClose}
-                footer={[
-                    <Button danger type="primary" key="close" onClick={handleClose}>
-                        Close
-                    </Button>
-                ]}
-            >
-                {selectedImage !== null && <img src={images[selectedImage]} alt={`Enlarged Image ${selectedImage}`} className="enlarged-image" />}
+        <div className='image-gallery-container'>
+            {images?.map((imageUrl, index) => (
+                <Col key={index} span={6}>
+                    {imageLoadingStates[index] && (
+                        <Spin className='loader' size='large' />
+                    )}
+                    <img
+                        src={imageUrl}
+                        alt={`Image ${index}`}
+                        className={
+                            'gallery-image ' +
+                            (imageLoadingStates[index] ? ' hidden' : '')
+                        }
+                        onClick={() => handleImageClick(index)}
+                        onLoad={() => {
+                            imageLoadingStates[index] = false
+                            setImageLoadingStates([...imageLoadingStates])
+                        }}
+                        onError={(e) => {
+                            e.target.src = noImage
+                            images[index] = noImage
+                        }}
+                    />
+                </Col>
+            ))}
+            <Modal open={visible} onCancel={handleClose} footer={null}>
+                {selectedImage !== null && (
+                    <img
+                        style={{ marginTop: '25px' }}
+                        src={images[selectedImage]}
+                        alt={`Enlarged Image ${selectedImage}`}
+                        className='enlarged-image'
+                    />
+                )}
             </Modal>
         </div>
-    );
-};
+    )
+}
 
-export default ImageGallery;
+export default ImageGallery
