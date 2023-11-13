09c8d1623031e3cfa9803c3282430b470671cf0d
Ahmed <ahmedmahmoudkzm@gmail.com>
2023-11-13 16:25:58 +0200

    Updated sprint 1 filteration to check the doctor's schedule

--- client/src/components/patient/ViewFamily/FamilyMemberCard.jsx
+++ client/src/components/patient/ViewFamily/FamilyMemberCard.jsx
@@ -1,5 +1,5 @@
 import { Button, message, Dropdown, Menu } from 'antd'
-import { InfoCircleFilled } from '@ant-design/icons'
+import { InfoCircleOutlined } from '@ant-design/icons'
 import './familyMemberCard.css'
 import axiosApi from '../../../utils/axiosApi'
 import ConditionalRender from '../../reusable/ConditionalRender/ConditionalRender'
@@ -29,72 +29,68 @@ const FamilyMemberCard = ({ member, relation, family, setFamily }) => {
 
     return (
         <div className='member-card'>
-            <h3>
-            {relation}
-            </h3>
-                
-                    <div>
-                        <strong>Name: </strong> {member?.name}
-                    </div>
-                    <div>
-                        <strong>Email: </strong> {member?.email}
-                    </div>
-                    <div>
-                        <strong>Phone Number: </strong> {member?.phoneNumber}
-                    </div>
-                    <div>
-                        <strong>Subscribed Package: </strong>{' '}
-                        <span>
-                            {member?.package?.name ??
-                                'Not subscribed to a package'}{' '}
-                        </span>
-                        <ConditionalRender condition={member?.package != null}>
-                            <Dropdown
-                                placement='right'
-                                overlay={
-                                    <Menu>
-                                        <Menu.Item>
-                                            {
-                                                <div className='package-info'>
-                                                    <p>
-                                                        Price:{' '}
-                                                        {member?.package?.price}
-                                                    </p>
-                                                    <p>
-                                                        Session Discount:{' '}
-                                                        {
-                                                            member?.package
-                                                                ?.sessionDiscount
-                                                        }
-                                                    </p>
-                                                    <p>
-                                                        Pharmacy Discount:{' '}
-                                                        {
-                                                            member?.package
-                                                                ?.medicineDiscount
-                                                        }
-                                                    </p>
-                                                    <p>
-                                                        Family Discount:{' '}
-                                                        {
-                                                            member?.package
-                                                                ?.familySubsDiscount
-                                                        }
-                                                    </p>
-                                                </div>
-                                            }
-                                        </Menu.Item>
-                                    </Menu>
-                                }>
-                                <InfoCircleFilled />
-                            </Dropdown>
-                        </ConditionalRender>
-                    </div>
-                    <div className='edit-buttons'>
-                        <Button danger onClick={handleCancelSubscirption}>
-                            Cancel Subscription
-                        </Button>
-                    </div>
+            <h3>{relation}</h3>
+
+            <div>
+                <strong>Name: </strong> {member?.name}
+            </div>
+            <div>
+                <strong>Email: </strong> {member?.email}
+            </div>
+            <div>
+                <strong>Phone Number: </strong> {member?.phoneNumber}
+            </div>
+            <div>
+                <strong>Subscribed Package: </strong>{' '}
+                <span>
+                    {member?.package?.name ?? 'Not subscribed to a package'}{' '}
+                </span>
+                <ConditionalRender condition={member?.package != null}>
+                    <Dropdown
+                        placement='right'
+                        overlay={
+                            <Menu>
+                                <Menu.Item>
+                                    {
+                                        <div className='package-info'>
+                                            <p>
+                                                Price: {member?.package?.price}
+                                            </p>
+                                            <p>
+                                                Session Discount:{' '}
+                                                {
+                                                    member?.package
+                                                        ?.sessionDiscount
+                                                }
+                                            </p>
+                                            <p>
+                                                Pharmacy Discount:{' '}
+                                                {
+                                                    member?.package
+                                                        ?.medicineDiscount
+                                                }
+                                            </p>
+                                            <p>
+                                                Family Discount:{' '}
+                                                {
+                                                    member?.package
+                                                        ?.familySubsDiscount
+                                                }
+                                            </p>
+                                        </div>
+                                    }
+                                </Menu.Item>
+                            </Menu>
+                        }>
+                        <InfoCircleOutlined />
+                    </Dropdown>
+                </ConditionalRender>
+            </div>
+            <div className='edit-buttons'>
+                <Button danger onClick={handleCancelSubscirption}>
+                    Cancel Subscription
+                </Button>
+            </div>
         </div>
     )
 }
